2230c8bf3e7786f5a22426a5218283539ce8ee96
Anil Makhijani <anil@users.noreply.github.com>
2014-10-02 15:46:25 -0400

    Interim change in changing to monger

--- src/timetracker/models.clj
+++ src/timetracker/models.clj
@@ -3,16 +3,13 @@
 
 (defn now [] (new java.util.Date))
 
-(defn post_processor [data] (assoc-in data [:project_id] "processed"))
-
 (defn create [task]
   (println (str "Added " task " at " (now) ))
   (mongo/insert! :tasks {:task task, :time (now), :project_id "N/A"}))
 
 (defn process []
   (println "start post process ...")
-  (let [data (mongo/fetch :tasks)
-        transformed-data (doall (map post_processor data))]
-        (mongo/update! :tasks data transformed-data :multiple true))
+  (let [data (mongo/fetch-one :tasks)]
+        (mongo/update! :tasks data (merge data {:project_id "processed"})))
   (println "... end  post process.")) 
 
